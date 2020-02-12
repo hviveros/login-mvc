da38db4850f81b15354491a9684c71316cd1368a
Humberto Viveros <humberto.viverosrios@gmail.com>
2020-02-12 20:09:11 -0300

    actualizacion de las cabeceras, menu y pie de pagina

---
+++ views/includes/cabecera.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Sistema Login de Accesos con PHP - MVC - PDO</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- styles -->
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/starter-template.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<body>
