34e8722a636f6fe296d175d5d6b466b877946acf
Humberto Viveros <humberto.viverosrios@gmail.com>
2020-02-15 12:37:26 -0300

    creacion del metodo inicio

--- controllers/InicioController.php
+++ controllers/InicioController.php
@@ -7,15 +7,15 @@ class InicioController {
 	#estableciendo las vistas
 	public function inicio() {
 		session_start();
-		if ( /*comparar variables de sesiones*/) {
+		//if ( /*comparar variables de sesiones*/) {
 	        require_once('./views/includes/cabecera.php');
 	        require_once('./views/includes/navbar.php');
 	        require_once('./views/paginas/inicio.php');
 	        require_once('./views/includes/pie.php');
-		} else{
-			header('Location: index.php?page=login');
-            die();
-		}
+		//} else{
+			//header('Location: index.php?page=login');
+            //die();
+		//}
 	}
 
 }
