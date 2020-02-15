8602449c070fcda3baa175d4ea007054a6d6b54f
Humberto Viveros <humberto.viverosrios@gmail.com>
2020-02-15 12:40:01 -0300

    creacion del item Cerrar Sesion, llamado del metodo CerrarSesion

--- views/includes/navbar.php
+++ views/includes/navbar.php
@@ -1,3 +1,12 @@
+<?php
+    require_once 'controllers/UsuarioController.php';
+    $usuario = new UsuarioController();
+
+    if (isset($_POST['salir'])) {
+        $usuario->cerrarSesion();
+    }
+?>
+
 	<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
 		<a class="navbar-brand" href="#"><?php echo NOMBRE_SISTEMA; ?></a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
@@ -9,8 +18,12 @@
 				<li class="nav-item active">
 					<a class="nav-link" href="index.php">Inicio <span class="sr-only">(current)</span></a>
 				</li>
+			</ul>
+			<ul class="navbar-nav text-right">
 				<li class="nav-item">
-					
+					<form action="" method="POST" name="logoutForm" id="logoutForm">
+						<button type="submit" class="btn btn-link nav-link" name="salir">Cerrar sesión</button>
+					</form>
 				</li>
 			</ul>
 		</div>
