0400f6443cee5c1553cbe22a3cab7a1dddaf41de
hviveros <humberto.viverosrios@gmail.com>
2020-02-16 18:06:15 -0300

    página Inicio, optimización del listado de páginas, y sus respectivas acciones

--- views/paginas/inicio.php
+++ views/paginas/inicio.php
@@ -7,62 +7,56 @@
 
 ?>
 
-	<main role="main" class="container">
+<main role="main" class="container">
 
-		<div class="starter-template">
-			<h1>Sistema Login con PHP - MVC - PDO</h1>
-			<hr>
-			<table class="table table-bordered">
-				<p>
-					<ul class="list-unstyled">
-						<li>#id: <?php echo $_SESSION['id_usuario']; ?></li>
-						<li>Nombre: <?php echo $_SESSION['nombre']; ?></li>
-						<li>Usuario: <?php echo $_SESSION['nick']; ?></li>
-					</ul>
-				</p>
-				<thead class="thead-dark">
-					<tr>
-						<th scope="col">#id</th>
-						<th scope="col">Paginas</th>
-						<th scope="col">Nivel</th>
-						<th scope="col">acciones</th>
+	<div class="starter-template">
+		<h1>Sistema Login con PHP - MVC - PDO</h1>
+		<hr>
+		<table class="table table-bordered">
+			<p>
+				<ul class="list-unstyled">
+					<li>#id: <?php echo $_SESSION['id_usuario']; ?></li>
+					<li>Nombre: <?php echo $_SESSION['nombre']; ?></li>
+					<li>Usuario: <?php echo $_SESSION['nick']; ?></li>
+				</ul>
+			</p>
+			<thead class="thead-dark">
+				<tr>
+					<th scope="col">Paginas</th>
+					<th scope="col">Acciones</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php
+					if (!empty($permisos)) {
+						foreach ($permisos as $r) { 
+
+							//si el nivel es 0, no muestra la página
+							if ($r['nivel'] > 0) {
+				?>
+					<tr>							
+						<td><?php echo $r['pagina'];?></td>
+						<td>
+							<?php //al ser el nivel > 0, está habilitado como mínimo para ver la pagina ?>
+							<a href="?page=<?php echo $r['pagina'];?>" class="btn btn-secondary">Lista</a>
+							<?php
+								//al ser el nivel > 1, está habilitado para insertar/editar registros de la pagina
+								if ($r['nivel'] > 1) {
+							?>
+								<a href="?page=<?php echo $r['pagina'];?>&action=insertar" class="btn btn-primary">Insertar</a>
+								<a href="?page=<?php echo $r['pagina'];?>&action=editar" class="btn btn-info">Editar</a>
+							<?php 
+								}
+								//al ser el nivel > 2, está habilitado hasta para eliminar registros de la pagina
+								if ($r['nivel'] > 2) {
+							?>
+								<a href="?page=<?php echo $r['pagina'];?>&action=eliminar" class="btn btn-danger">Eliminar</a>
+							<?php } ?>
+						</td>
 					</tr>
-				</thead>
-				<tbody>
-					<?php
-						if (!empty($permisos)) {
-							foreach ($permisos as $r) { 
-					?>
-						<tr>
-							<th scope="row"><?=$r['id_permiso'];?></th>
-							<td><?=$r['pagina'];?></td>
-							<td><?=$r['nivel'];?></td>
-							<td>
-								<?php 
-									if ($r['nivel'] > 0) {
-								?>
-									<a href="?page=insertar&id=" class="btn btn-secondary">Lista</a>
-								<?php 
-									}
+				<?php	} } } ?>
+			</tbody>
+		</table>
+	</div>
 
-									if ($r['nivel'] > 1) {
-								?>
-									<a href="?page=insertar&id=" class="btn btn-primary">Insertar</a>
-									<a href="?page=editar&id=" class="btn btn-info">Editar</a>
-								<?php 
-									}
-
-									if ($r['nivel'] > 2) {
-								?>
-									<a href="?page=eliminar&id=" class="btn btn-danger">Eliminar</a>
-								<?php 
-									}
-								?>
-							</td>
-						</tr>
-					<?php } } ?>
-				</tbody>
-			</table>
-		</div>
-
-	</main><!-- /.container -->
+</main><!-- /.container -->
