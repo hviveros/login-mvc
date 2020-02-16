46b505d71215ae2de5fb88b198f7d00e6e3b7aa0
hviveros <humberto.viverosrios@gmail.com>
2020-02-16 18:01:42 -0300

    estableciendo condiciones para mostrar la pagina inicio

--- controllers/InicioController.php
+++ controllers/InicioController.php
@@ -7,15 +7,15 @@ class InicioController {
 	#estableciendo las vistas
 	public function inicio() {
 		session_start();
-		//if ( /*comparar variables de sesiones*/) {
+		if ( isset($_SESSION['id_usuario']) && $_SESSION['login'] == 'ok') {
 	        require_once('./views/includes/cabecera.php');
 	        require_once('./views/includes/navbar.php');
 	        require_once('./views/paginas/inicio.php');
 	        require_once('./views/includes/pie.php');
-		//} else{
-			//header('Location: index.php?page=login');
-            //die();
-		//}
+		} else{
+			header('Location: index.php?page=login');
+            die();
+		}
 	}
 
 }
