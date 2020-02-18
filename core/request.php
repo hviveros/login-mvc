18c0524287507403b974e07734e7ac2802a2f384
hviveros <humberto.viverosrios@gmail.com>
2020-02-17 23:20:49 -0300

    actualización del archivo request.php

--- core/request.php
+++ core/request.php
@@ -6,10 +6,14 @@ $page = $_GET['page'];
 
 if (!empty($page)) {
 	#http://login-mvc/index.php?page=pelicula
+	#http://login-mvc/index.php?page=pelicula-insertar
+	#http://login-mvc/index.php?page=pelicula-editar
+	#http://login-mvc/index.php?page=pelicula-eliminar
 	$data = array(
-		'login' => array('model' => 'UsuarioModel', 'view' => 'login', 'controller' => 'UsuarioController'),
-		'inicio' => array('model' => 'UsuarioModel','view' => 'inicio', 'controller' => 'InicioController'), 
-		'pelicula' => array('model' => 'UsuarioModel','view' => 'pelicula', 'controller' => 'PeliculaController'), 
+		'login' 	=> array('model' => 'UsuarioModel', 'view' => 'login', 'controller' => 'UsuarioController'),
+		'inicio' 	=> array('model' => 'UsuarioModel','view' => 'inicio', 'controller' => 'InicioController'), 
+		'error' 	=> array('model' => 'UsuarioModel','view' => 'error', 'controller' => 'InicioController'), 
+		'pelicula' 	=> array('model' => 'PeliculaModel','view' => 'pelicula', 'controller' => 'PeliculaController'), 
 	);
 
 	foreach($data as $key => $components) {
